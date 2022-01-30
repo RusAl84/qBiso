9ff68775982d18a635e272a1ab6ac3c5a69a7656
Aleksey <rusal@bk.ru>
2022-01-30 23:48:00 +0300

    add login panel

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,31 +1,35 @@
 <template>
   <q-page class="flex flex-center ">
-    <q-card style="flex-directiosn: row;">
-    <img  
-    alt="hedgehog"
-    src="~assets/education.png" 
-    style="width: 200px; height: 200px"/>
-    <q-card>
-      <q-card-section>
-        <q-input></q-input>
-      </q-card-section>
-    </q-card>
-    <q-card>
-      <q-card-section>
-        Перевод
-      </q-card-section>
-    </q-card>
-        <q-card  >
-      <q-card-section>
-        <q-btn></q-btn>
-      </q-card-section>
-    </q-card>
+    <q-card bordered>
+    <div class="login-panel">
+    <q-card-section>
+      <img  
+      alt="hedgehog"
+      src="~assets/education.png" 
+      style="width: 150px; height: 150px"/>
+    </q-card-section>
+    <q-card-section>
+        <q-card bordered>
+          <q-card-section>
+            <q-input></q-input>
+          </q-card-section>
+          <q-card-section>
+            Перевод
+          </q-card-section>
+        </q-card>
+    </q-card-section>
+    </div>
+    <div>
+    <q-card-section>
+        <q-btn class="full-width">кнопка</q-btn>
+    </q-card-section>
+    </div>
     </q-card>
     
     <div v-if:="$store.state.ezh.userName.length>1">
-    <div >
+    <div>
     <h4>Привет меня зовут <br> {{$store.state.ezh.userName}}</h4>
-   </div>
+    </div>
     <img  
     alt="hedgehog"
     src="~assets/hedgehog.png" 
@@ -49,3 +53,9 @@ export default defineComponent({
   }
 })
 </script>
+<style lang="scss" scoped>
+  .login-panel{
+    display: flex;
+    flex-direction: row;
+  }
+</style>
